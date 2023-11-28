0471ef19d36bc46d2c3771fcf78cee03e1114ac4
timkogas <timkogas@gmail.com>
2023-11-28 12:05:44 +0300

    prepare stats

--- src/routes/ejs/EjsAdminStatsPlatforms.ts
+++ src/routes/ejs/EjsAdminStatsPlatforms.ts
@@ -13,68 +13,23 @@ export default class EjsAdminStatsPlatforms {
     private _app: core.Express;
 
     private async _fetchUserStats(): Promise<any[]> {
-        const statsResult = await Db.query(`
-            SELECT
-                platform,
-                channel,
-                creative,
-                SUM(start) AS total_launches,
-                COUNT(DISTINCT id) AS unique_users,
-                SUM(web_app) AS total_web_app_launches,
-                COUNT(DISTINCT CASE WHEN web_app > 0 THEN id END) AS unique_web_app_users,
-                COUNT(DISTINCT CASE WHEN final = 1 THEN id END) AS game_sessions
-            FROM
-                users
-            GROUP BY
-                platform, channel, creative;
+        const platformsStats = await Db.query(`
+        SELECT
+            platform AS id,
+            SUM(start) AS total_launches,
+            COUNT(DISTINCT id) AS unique_users,
+            SUM(web_app) AS total_web_app_launches,
+            COUNT(DISTINCT CASE WHEN web_app > 0 THEN id END) AS unique_web_app_users,
+            COUNT(DISTINCT CASE WHEN final = 1 THEN id END) AS game_sessions
+        FROM
+            users
+        GROUP BY
+            platform;
         `);
 
-        Logger.debug('statsResult', JSON.stringify(statsResult))
+        Logger.debug('platformsStats', JSON.stringify(platformsStats))
+
         const groupedStats: any[] = [];
-        statsResult.forEach((row: any) => {
-            if (row.platform !== null) {
-                let platformObj = groupedStats.find((platform) => platform.id === row.platform);
-
-                if (!platformObj) {
-                    platformObj = {
-                        id: row.platform,
-                        total_launches: row.total_launches,
-                        unique_users: row.unique_users,
-                        total_web_app_launches: row.total_web_app_launches,
-                        unique_web_app_users: row.unique_web_app_users,
-                        game_sessions: row.game_sessions,
-                        channels: [],
-                    };
-
-                    groupedStats.push(platformObj);
-                }
-
-                let channelObj = platformObj.channels.find((channel) => channel.id === row.channel);
-
-                if (!channelObj) {
-                    channelObj = {
-                        id: row.channel,
-                        total_launches: row.total_launches,
-                        unique_users: row.unique_users,
-                        total_web_app_launches: row.total_web_app_launches,
-                        unique_web_app_users: row.unique_web_app_users,
-                        game_sessions: row.game_sessions,
-                        creatives: [],
-                    };
-
-                    platformObj.channels.push(channelObj);
-                }
-
-                channelObj.creatives.push({
-                    id: row.creative,
-                    total_launches: row.total_launches,
-                    unique_users: row.unique_users,
-                    total_web_app_launches: row.total_web_app_launches,
-                    unique_web_app_users: row.unique_web_app_users,
-                    game_sessions: row.game_sessions,
-                });
-            }
-        });
 
         return groupedStats;
     }
@@ -99,18 +54,19 @@ export default class EjsAdminStatsPlatforms {
 
 
 
-const d = [
-    {
-        "id": "tg", "total_launches": "5", "unique_users": 3, "total_web_app_launches": "9", "unique_web_app_users": 1, "game_sessions": 0,
-        "channels": [
-            {
-                "id": "il", "total_launches": "2", "unique_users": 2, "total_web_app_launches": "0", "unique_web_app_users": 0, "game_sessions": 0,
-                "creatives": [{ "id": "post", "total_launches": "2", "unique_users": 2, "total_web_app_launches": "0", "unique_web_app_users": 0, "game_sessions": 0 }]
-            },
+// const d = [
+//     {
+//         "id": "tg", "total_launches": "5", "unique_users": 3, "total_web_app_launches": "9", "unique_web_app_users": 1, "game_sessions": 0,
+//         "channels": [
+//             {
+//                 "id": "il", "total_launches": "2", "unique_users": 2, "total_web_app_launches": "0", "unique_web_app_users": 0, "game_sessions": 0,
+//                 "creatives": [{ "id": "post", "total_launches": "2", "unique_users": 2, "total_web_app_launches": "0", "unique_web_app_users": 0, "game_sessions": 0 }]
+//             },
 
-            {
-                "id": "sblr", "total_launches": "3", "unique_users": 1, "total_web_app_launches": "8", "unique_web_app_users": 1, "game_sessions": 0, "creatives":
-                    [{ "id": "post", "total_launches": "3", "unique_users": 1, "total_web_app_launches": "8", "unique_web_app_users": 1, "game_sessions": 0 }]
-            }
-        ]
-    }]
+//             {
+//                 "id": "sblr", "total_launches": "3", "unique_users": 1, "total_web_app_launches": "8", "unique_web_app_users": 1, "game_sessions": 0,
+//                 "creatives":
+//  [{ "id": "post", "total_launches": "3", "unique_users": 1, "total_web_app_launches": "8", "unique_web_app_users": 1, "game_sessions": 0 }]
+//             }
+//         ]
+//     }]
